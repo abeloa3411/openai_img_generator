bc1728932917089c1dca7a11ce5d6f32e7c4fe29
abel <abelw34@gmail.com>
2022-12-13 15:41:03 +0300

    public

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,43 @@ function onSubmit(e) {
     alert("Enter a description");
     return;
   }
-  console.log(prompt, size);
+
+  generateImage(prompt, size);
+}
+
+async function generateImage(prompt, size) {
+  try {
+    showSpinner();
+
+    document.getElementById("prompt").value = "";
+
+    const response = await fetch("/openai/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt, size }),
+    });
+
+    if (!response.ok) {
+      removeSpinner();
+      throw new Error("Image could not be generated");
+    }
+
+    const data = await response.json();
+
+    document.querySelector("#image").src = data;
+    removeSpinner();
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+function removeSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
 }
 
 document.querySelector("#form").addEventListener("submit", onSubmit);
